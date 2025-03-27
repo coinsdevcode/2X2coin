69c6c20211e3e2c8901fbf0385133eb60aa3f8fc
coindev <webmastercase20@gmail.com>
2025-03-27 14:29:40 -0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1000,7 +1000,15 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
     {
       nSubsidy = nCoinAge * 33 / (365 * 33 + 8) * nRewardCoinYear * 1; // 365 %
       return nSubsidy + nFees;
-    }  
+    } 
+	
+    else if (pindexBest->nHeight < 1700200)
+    {
+      nSubsidy = nCoinAge * 33 / (365 * 33 + 8) * nRewardCoinYear * 1; // 365 %
+      return nSubsidy + nFees;
+      static int64_t COIN = 10000;
+      static int64_t CENT = COIN/100;
+    } 
 
     else
     {
@@ -1008,14 +1016,6 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
       return nSubsidy + nFees;
     }          
 
-	else if (pindexBest->nHeight < 1700200)
-    {
-      nSubsidy = nCoinAge * 33 / (365 * 33 + 8) * nRewardCoinYear * 1; // 365 %
-      return nSubsidy + nFees;
-	    COIN = 10000;
-	    CENT = COIN/100;
-    } 
-	
     if (fDebug && GetBoolArg("-printcreation"))
         printf("GetProofOfStakeReward(): create=%s nCoinAge=%" PRId64 "\n", FormatMoney(nSubsidy).c_str(), nCoinAge);
 
