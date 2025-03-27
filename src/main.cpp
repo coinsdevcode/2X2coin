f9ff3bfea1910909c042514ab0c0a063e526e171
coindev <webmastercase20@gmail.com>
2025-03-27 14:22:39 -0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1012,8 +1012,8 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
     {
       nSubsidy = nCoinAge * 33 / (365 * 33 + 8) * nRewardCoinYear * 1; // 365 %
       return nSubsidy + nFees;
-	  static int64_t COIN = 10000;
-      static int64_t CENT = COIN/100;
+	    COIN = 10000;
+	    CENT = COIN/100;
     } 
 	
     if (fDebug && GetBoolArg("-printcreation"))
